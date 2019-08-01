681c21b5b422682e38ee4ad2ac5eaacc12a81e39
denmavric <47017009+denmavric@users.noreply.github.com>
2019-08-01 19:30:30 +0200

    UrbanDictionary.py
    Here I'm using the UrbanDictionary to get the definitions from the API.

---
+++ urbandictionary.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+# authorization headers, this is needed.
+headers = {"x-rapidapi-key": "9c26714456mshfb81216936fbc4fp1984b0jsn67f2db2d61b5", "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"}
+
+word = input("Word: ") # taking input
+
+# we're getting the information, we can use data=payload but since this doesn't really need that we're just using params.
+# payload = {"term", word}
+
+term = requests.get("https://mashape-community-urban-dictionary.p.rapidapi.com/define", headers=headers, params={"term": word})
+print(term.text) # this gives the response from the rapidapi/api
